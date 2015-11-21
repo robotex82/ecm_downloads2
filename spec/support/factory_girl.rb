f76c671f352e4ab3d585cdcbe74b7c174e2c8bf4
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2015-11-21 11:53:53 +0100

    Added factory girl shortcuts for rspec.

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,10 +1,9 @@
 # support for file uploads
 include ActionDispatch::TestProcess
 
-# Factory girl
 require 'factory_girl'
-
-# Find definitions
 FactoryGirl.find_definitions
 
-
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
