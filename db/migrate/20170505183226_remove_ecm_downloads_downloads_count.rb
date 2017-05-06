5beed50f5803fa9f71e0ebc0dff12caa000930b9
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-05-06 19:30:41 +0200

    Rename migrations with too long filenames.

--- db/migrate/20170505183226_remove_ecm_downloads_downloads_count.rb
+++ db/migrate/20170505183226_remove_ecm_downloads_downloads_count.rb
@@ -1,4 +1,4 @@
-class RemoveEcmDownloadsDownloadsCountFromEcmDownloadsDownloadCategories < ActiveRecord::Migration[4.2]
+class RemoveEcmDownloadsDownloadsCount < ActiveRecord::Migration[4.2]
   def change
     remove_column :ecm_downloads_download_categories, :ecm_downloads_downloads_count
   end
