c02d49961b1b467db5a96abd9f1ccbfd2f059c4c
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-05-07 17:17:56 +0200

    Localize routes by default.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Ecm::Downloads::Engine.routes.draw do
   localized do
-    resources :download_categories, only: [:index, :show]
-    resources :downloads,  only: [:index, :show] do
-      get :download, on: :member
+    scope :ecm_downloads do
+      resources :download_categories, only: [:index, :show]
+      resources :downloads,           only: [:index, :show] do
+        get :download, on: :member
+      end
+      root to: 'download_categories#index'
     end
   end
 end
