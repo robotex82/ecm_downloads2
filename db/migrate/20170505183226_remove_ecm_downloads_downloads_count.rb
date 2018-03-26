ebb868dc54371f1082455bad4fac3b6420a352e5
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-03-26 20:57:35 +0200

    Make migration reversible.

--- db/migrate/20170505183226_remove_ecm_downloads_downloads_count.rb
+++ db/migrate/20170505183226_remove_ecm_downloads_downloads_count.rb
@@ -1,5 +1,6 @@
 class RemoveEcmDownloadsDownloadsCount < ActiveRecord::Migration[4.2]
   def change
-    remove_column :ecm_downloads_download_categories, :ecm_downloads_downloads_count
+    remove_column :ecm_downloads_download_categories, :ecm_downloads_downloads_count, :integer
+  end
   end
 end
